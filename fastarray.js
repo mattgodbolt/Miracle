802c40c7bb6c408e74e9754a5648aff46569d91c
Matt Godbolt <mgodbolt@drwholdings.com>
2011-06-21 21:25:05 -0400

    Add a shim layer for Uint*arrays

---
+++ fastarray.js
@@ -0,0 +1,9 @@
+// Workarounds for older browsers without faster arrays.
+if (typeof(Uint32Array) === 'undefined') {
+	function Uint32Array(size) {
+		return [];
+	}
+	function Uint8Array(size) {
+		return [];
+	}
+}
